28240bd5ec1f08026d247dbc98f28b7769979144
Yamiraj <yamiraj.dev@gmail.com>
2023-05-11 20:46:04 +0800

    Learnt arrays more, created an application to print out an array item based on entered index number.

--- ArraysApp/Arrays/Program.cs
+++ ArraysApp/Arrays/Program.cs
@@ -1,5 +1,31 @@
 ﻿
+// 0 based counting - 0, 1, 2, 3, 4
 
-string[] firstNames = new string[5]; // this array now holds 5 first names
 
-firstNames[0] = "Tim";
+/*string[] firstNames = new string[5]; // this array now holds 5 first names
+
+firstNames[0] = "Tim"; // position 0 refers to the first item in an array
+firstNames[1] = "Mus";
+firstNames[2] = "Pia";
+firstNames[4] = "Jaysea";
+
+Console.WriteLine($"The firstNames are {firstNames[0]}, {firstNames[1]}, {firstNames[2]}, {firstNames[4]}");
+
+firstNames[0] = "Bob";
+
+Console.WriteLine(firstNames[0]);*/
+
+// we cannot go to store [5] as this exceeds our array size 
+
+
+string data = "Tim,Mus,Pia,Jaysea,Juno";
+string[] firstNames = data.Split(','); // you should use single quotes for identifying a char, double quotes are for string values
+
+Console.WriteLine(firstNames[4]);
+
+Console.WriteLine(firstNames.Length);
+
+// another way of declaring an array
+
+string[] lastNames = new string[] { "Corey", "Gonzales", "Po" };
+int[] ages = new int[] { 2, 3, 4 };
---
+++ ArraysHomeworkApp/ArraysHomework/Program.cs
@@ -0,0 +1,24 @@
+﻿
+bool isValidIndex;
+int indexNumber = 0;
+string[] firstNames = new string[3];
+firstNames[0] = "Mus";
+firstNames[1] = "Jaysea";
+firstNames[2] = "Pia";
+
+
+
+do {
+    Console.Write("Please enter an index: ");
+    string indexText = Console.ReadLine();
+    isValidIndex = int.TryParse(indexText, out indexNumber);
+
+    if (isValidIndex == false || (indexNumber > 2 || indexNumber < 0))
+
+    {
+        Console.WriteLine("Please enter a valid index number within range (0-2)");
+    }
+
+} while ((isValidIndex == false) || (indexNumber > 2 || indexNumber < 0));
+
+Console.WriteLine($"The first name of this index is: {firstNames[indexNumber]}");
