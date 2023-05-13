88430051d97c6189d4c8c0df559d97e20dabc5e0
Yamiraj <yamiraj.dev@gmail.com>
2023-05-13 17:36:48 +0800

    Learned about lists, made a program to add names to a list and print the count of items.

---
+++ ListsApp/Lists/Program.cs
@@ -0,0 +1,25 @@
+﻿// lists are an upgrade to arrays, they are a more modern way of storing data
+
+
+// string[] firstNames = new string[5];
+List<string> firstNames = new List<string>(); // list of T, T means any type you want
+
+firstNames.Add("Mus");
+firstNames.Add("Jaysea");
+firstNames.Add("Pia");
+firstNames.Add("Mulan");
+
+// it is also 0 based counting like arrays
+Console.WriteLine(firstNames[firstNames.Count - 1]);
+
+List<int> ages = new List<int>();
+ages.Add(1);
+ages.Add(2);
+ages.Add(3);
+
+// List<T> - generic
+
+string data = "Gonzales, Ocares, Po";
+List<string> lastNames = data.Split(',').ToList(); // we convert the data split from an array to list
+lastNames.Add("Jaafar"); // and then add a last name to the end of the list of last names
+
---
+++ ListsHomeworkApp/ListsHomework/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Console.Write("Please enter your first name: ");
+string firstName = Console.ReadLine();
+
+List<string> firstNames = new List<string>();
+string data = firstName;
+firstNames = data.Split(',').ToList();
+
+Console.WriteLine(firstNames.Count);*/
+
+List<string> firstNames = new List<string>();
+string firstName;
+do
+{
+    Console.Write("Exit or add name: ");
+    firstName = Console.ReadLine();
+
+    if (firstName.ToLower() == "exit")
+    {
+        Console.WriteLine(firstNames.Count);
+    }
+    else
+    {
+        firstNames.Add(firstName); 
+    }
+
+
+
+} while (firstName.ToLower() != "exit");
+
