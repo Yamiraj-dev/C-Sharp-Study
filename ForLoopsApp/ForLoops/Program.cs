41918500595693208a587f4f9c88e0396ac297b0
Yamiraj <yamiraj.dev@gmail.com>
2023-05-15 03:48:03 +0800

    Learnt about for loops and created an app to split a list of names entered by a user and print a greeting for each name in the list.

---
+++ ForLoopsApp/ForLoops/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+// int i is a counter, which starts at position 0
+// second condition checks if i is less than the length of something
+// add 1 to i is the last condition
+
+/*for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"The value of i is {i}");
+}*/
+
+/*string data = "Mus,Mulan,Jaysea,Pia,Juno";
+List<string> firstNames = data.Split(',').ToList();
+
+for (int i = 0; i < firstNames.Count; i++)
+{
+    Console.WriteLine($"{firstNames[i]} is present.");
+}*/
+
+List<decimal> charges = new();
+
+charges.Add(23.78M);
+charges.Add(19.98M);
+charges.Add(194M);
+
+decimal total = 0;
+
+for (int i = 0; i < charges.Count; i++)
+{
+    total += charges[i];
+    Console.WriteLine(charges[i]);
+}
+
+Console.WriteLine($"Our total charges is {total}");
---
+++ ForLoopsHomeworkApp/ForLoopsHomework/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.ComponentModel;
+
+string firstNamesText;
+Console.WriteLine("Enter a list of names separated by a comma with no spaces.");
+Console.Write("Please enter the names: ");
+firstNamesText = Console.ReadLine();
+
+List<string> firstNames = firstNamesText.Split(',').ToList();
+
+for (int i = 0; i < firstNames.Count; i++)
+{
+    Console.WriteLine($"Hello, {firstNames[i]}.");
+}
