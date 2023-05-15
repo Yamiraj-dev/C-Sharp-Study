c484144b269d217ddb4cfe8fc12d8a86d7175a3a
Yamiraj <yamiraj.dev@gmail.com>
2023-05-15 18:04:29 +0800

    Learnt about methods with DRY & SRP principles. To do: Method parameters

---
+++ MethodsApp/Methods/ConsoleMessages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods;
+
+// we add static before class to work with our program.cs console app
+
+// public - anyone can call this method
+// internal - anyone inside the project can call this method
+// private - anyone inside this scope can call this method
+public static class ConsoleMessages
+{
+    // naming a method uses Pascal Casing (every word's first letter is capitalized)
+    public static void SayHello()
+    {
+        Console.WriteLine("Hello there!");
+    }
+
+    public static void SayGoodbye()
+    {
+        Console.WriteLine("Goodbye my dude.");
+    }
+}
+
---
+++ MethodsApp/Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using Methods;
+// we can shorthen samplemethods.sayhello by adding :
+// using static Methods.SampleMethods; however we might run into ambiguous calls the more we shorten calls
+
+// the context of this is in static void main
+// static allows us to call methods without having to instatiate 
+
+
+// DRY - Don't Repeat Yourself 
+// Instead of copying code logic in multiple areas and it bugs out, we can refactor the code in one place so it works out in other areas
+// Working on a method is better than fixing copied code in multiple areas
+
+// SOLID - S (SRP) single responsbility principle 
+ConsoleMessages.SayHello();
+ConsoleMessages.SayGoodbye();
