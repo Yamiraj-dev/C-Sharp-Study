165346eeef08a5bb53fe2f30beefca48d10ce02c
Yamiraj <yamiraj.dev@gmail.com>
2023-05-15 15:33:24 +0800

    Learnt ForEach loops, made a console app that takes in first names and greets each one using this loop.

---
+++ ForEachHomeworkApp/ForEachHomework/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string firstName;
+List<string> firstNames = new();
+
+do
+{
+    Console.Write("Please enter your first name or type exit: ");
+    firstName = Console.ReadLine();
+
+    firstNames.Add(firstName);
+
+} while (firstName.ToLower() != "exit");
+
+foreach (string name in firstNames)
+{
+    Console.WriteLine($"Hello, {name}");
+}
---
+++ ForEachLoopsApp/ForEachLoops/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+string data = "Mus,Mulan,Jaysea,Pia";
+List<string> firstNames = data.Split(',').ToList();
+
+/*for (int i = 0; i < firstNames.Count; i++)
+{
+
+}*/
+
+// creates a string variable firstName for every element in the list of firstNames
+foreach (string firstName in firstNames)
+{
+    Console.WriteLine(firstName);
+}
